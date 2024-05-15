35bcaae5a0e052a2e4a8ce720b56799a9706af75
jojorsg <josueroberto099@gmail.com>
2024-05-15 17:19:57 -0300

    Add recursividade

---
+++ src/recursividade/ProgramacaoDinamica.java
@@ -0,0 +1,32 @@
+package recursividade;
+
+public class ProgramacaoDinamica {
+
+    private static final int MAX_ELEMENTS = 100;
+
+    private static final long[] elementosFat = new long[MAX_ELEMENTS];
+
+    public static long encontrarFatorialPD(int n) {
+        for (int i = 0; i < MAX_ELEMENTS ; i++) {
+            elementosFat[i] = -1;
+        }
+        return encontrarFatorialPD(n);
+    }
+
+    public static long encontrarFatorial(int n) {
+        if (elementosFat[n] == -1) {
+            if (n <+ 1) {
+                elementosFat[n] = n;
+            } else {
+                elementosFat[n] = encontrarFatorial(n) * encontrarFatorial(n-1);
+            }
+        }
+        return elementosFat[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+
+        System.out.println("Fatorial de " + n + " é: " + encontrarFatorialPD(n));
+    }
+}
---
+++ src/recursividade/Recursao.java
@@ -0,0 +1,17 @@
+package recursividade;
+
+public class Recursao {
+
+    public static int encontrarFatorial(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        return encontrarFatorial(n) * encontrarFatorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+
+        System.out.println("Fatorial de " + n + " é: " + encontrarFatorial(n));
+    }
+}
